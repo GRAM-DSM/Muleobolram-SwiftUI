59a6fc7ff784a5a03919653038f69186465231e6
rlarldud1234 <rlarldud6482@naver.com>
2022-04-18 23:35:09 +0900

    📄 :: 프로젝트 세팅

---
+++ Project.swift
@@ -0,0 +1,26 @@
+import ProjectDescription
+
+let project = Project(
+    name: "<ProjectName>",
+    organizationName: "<OrganizationName>",
+    targets: [
+        Target(
+            name: "<TargetName>",
+            platform: .iOS,
+            product: .app,
+            bundleId: "<OrganizationName.TargetName>",
+            deploymentTarget: .iOS(targetVersion: "13.0", devices: .iphone),
+            infoPlist: .file(path: Path("SupportingFiles/Info.plist")),
+            sources: ["Sources/**"],
+            resources: [
+                "Resources/**"
+            ],
+            scripts: [
+                .pre(
+                    script: "${PODS_ROOT}/SwiftLint/swiftlint",
+                    name: "SwiftLint"
+                )
+            ]
+        )
+    ]
+)
