86e81cb9de249f4e33b39bb45521d14e13e01fd4
rlarldud1234 <rlarldud6482@naver.com>
2022-04-19 09:48:31 +0900

    📄 :: 프로젝트 세팅

--- Project.swift
+++ Project.swift
@@ -1,14 +1,14 @@
 import ProjectDescription
 
 let project = Project(
-    name: "<ProjectName>",
-    organizationName: "<OrganizationName>",
+    name: "Muleobolram",
+    organizationName: "com.GRAM-DSM",
     targets: [
         Target(
-            name: "<TargetName>",
+            name: "Muleobolram",
             platform: .iOS,
             product: .app,
-            bundleId: "<OrganizationName.TargetName>",
+            bundleId: "com.GRAM-DSM.Muleobolram",
             deploymentTarget: .iOS(targetVersion: "13.0", devices: .iphone),
             infoPlist: .file(path: Path("SupportingFiles/Info.plist")),
             sources: ["Sources/**"],
