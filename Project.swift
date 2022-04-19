1510130d73c2aef451fab7055d602aa3a7e0b118
rlarldud1234 <rlarldud6482@naver.com>
2022-04-19 15:06:45 +0900

    ⚡️ :: version 14.0부터로 바꿈

--- Project.swift
+++ Project.swift
@@ -9,7 +9,7 @@ let project = Project(
             platform: .iOS,
             product: .app,
             bundleId: "com.GRAM-DSM.Muleobolram",
-            deploymentTarget: .iOS(targetVersion: "13.0", devices: .iphone),
+            deploymentTarget: .iOS(targetVersion: "14.0", devices: .iphone),
             infoPlist: .file(path: Path("SupportingFiles/Info.plist")),
             sources: ["Sources/**"],
             resources: [
