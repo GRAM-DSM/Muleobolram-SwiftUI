0503763f3d9dbff48a14b84ad404348be6a8222d
rlarldud1234 <rlarldud6482@naver.com>
2022-04-20 13:17:29 +0900

    ⚡️ :: MoyaProvider + requestPublishre, requestVoidPublisher

---
+++ Sources/Presentation/Data/Extension/MoyaProvider.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+import Combine
+import Moya
+
+extension MoyaProvider {
+
+    func requestPublisher<T: Decodable>(
+        _ target: Target,
+        _ model: T.Type
+    ) -> AnyPublisher<T, NetworkingError> {
+        return self.requestPublisher(target)
+            .map(T.self)
+            .mapError { NetworkingError($0) }
+            .eraseToAnyPublisher()
+    }
+
+    func requestVoidPublisher(_ target: Target) -> Future<Void, NetworkingError> {
+        return Future<Void, NetworkingError> { _ in
+            self.requestPublisher(target)
+            .map { _ in return }
+            .mapError { NetworkingError($0) }
+        }
+    }
+
+}
