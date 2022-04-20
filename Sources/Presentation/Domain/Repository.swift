1da76cd2fae344d80e10cfa829dbdb04c9584b8a
rlarldud1234 <rlarldud6482@naver.com>
2022-04-20 13:18:34 +0900

    ⚡️ :: Domain Layer Repository

---
+++ Sources/Presentation/Domain/Repository.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+import Combine
+
+protocol Repository {
+    // auth
+    func login(_ id: String, _ password: String) -> Future<Void, NetworkingError>
+    func signup(_ id: String, _ password: String, _ name: String) -> Future<Void, NetworkingError>
+    func checkId(_ id: String) -> Future<Void, NetworkingError>
+
+    // community
+    func postCommunity(_ title: String, _ content: String) -> Future<Void, NetworkingError>
+    func fetchCommunity() -> AnyPublisher<[Community], NetworkingError>
+    func deletCommunity(_ postId: Int) -> Future<Void, NetworkingError>
+
+    // comment
+    func postComment(_ postId: Int, _ content: String) -> Future<Void, NetworkingError>
+    func fetchComment(_ postId: Int) -> AnyPublisher<[Comment], NetworkingError>
+}
