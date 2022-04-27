e1e73a61405c3c114b0a7d5c74677757fd076ee0
rlarldud1234 <rlarldud6482@naver.com>
2022-04-27 19:46:58 +0900

    ⚡️ :: Color 추가

--- Sources/Presentation/Extension/Color.swift
+++ Sources/Presentation/Extension/Color.swift
@@ -4,4 +4,5 @@ extension Color {
     static let txtField = Self.init(.init(red: .infinity, green: .infinity, blue: .infinity, alpha: 0.8))
     static let authButton = Self("authButton")
     static let moveSignupTxt = Self("moveSignupTxt")
+    static let txtTitle = Self("txtTitle")
 }
