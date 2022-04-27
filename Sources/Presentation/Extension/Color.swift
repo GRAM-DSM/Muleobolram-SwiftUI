ea731d761f5f3dfd8eec2eff4bfeca4c7c070b8d
rlarldud1234 <rlarldud6482@naver.com>
2022-04-27 19:05:51 +0900

    ⚡️ :: Color 추가하기 및 Image

---
+++ Sources/Presentation/Extension/Color.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+extension Color {
+    static let txtField = Self.init(.init(red: .infinity, green: .infinity, blue: .infinity, alpha: 0.8))
+    static let authButton = Self("authButton")
+    static let moveSignupTxt = Self("moveSignupTxt")
+}
---
+++ Sources/Presentation/Extension/Image.swift
@@ -0,0 +1,5 @@
+import SwiftUI
+
+extension Image {
+    static let backgroundImage = Self.init(uiImage: .init(named: "backgroundImage")!)
+}
